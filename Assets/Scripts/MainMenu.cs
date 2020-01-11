3d811b26ff9a65f6c347f52c99bb7935128115f6
bbudian <bbudian0@gmail.com>
2020-01-11 14:19:11 -0500

    Setup basic main menu and scene transition

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public EventHandler handler;
+
+    [SerializeField] 
+    private Button m_PlayButton;
+    [SerializeField]
+    private Button m_OptionsButton;
+    [SerializeField]
+    private Button m_QuitButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_PlayButton.onClick.AddListener(HandlePlayClick);
+        m_OptionsButton.onClick.AddListener(HandleOptionsClick);
+        m_QuitButton.onClick.AddListener(HandleQuitClick);
+    }
+
+    private void HandleOptionsClick()
+    {
+    }
+
+    private void HandleQuitClick()
+    {
+    }
+
+
+    private void HandlePlayClick()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
