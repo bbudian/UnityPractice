6909506bbfefb09c810d3a741775822f3aace0b4
bbudian <bbudian0@gmail.com>
2020-01-11 14:49:37 -0500

    Setup In Game menu

---
+++ Assets/Scripts/InGameMenu.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class InGameMenu : MonoBehaviour
+{
+    [SerializeField] private Button m_ResumeButton;
+    [SerializeField] private Button m_OptionsButton;
+    [SerializeField] private Button m_QuitButton;
+    [SerializeField] private GameObject m_MenuObject;
+
+    public void Show(bool isVisible)
+    {
+        m_MenuObject.SetActive(isVisible);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_ResumeButton.onClick.AddListener(HandleResumeClick);
+        m_OptionsButton.onClick.AddListener(HandleOptionsClick);
+        m_QuitButton.onClick.AddListener(HandleQuitClick);
+        Show(false);
+    }
+
+    private void HandleOptionsClick(){}
+
+    private void HandleQuitClick()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    private void HandleResumeClick()
+    {
+        Show(false);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private InGameMenu m_InGameMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonUp("Menu"))
+        {
+            m_InGameMenu.Show(true);
+        }
+
+    }
+}
